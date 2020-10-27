e564f90dda93589b84200c36de368b5cd68a72f2
fareezj <fareez.reddy@gmail.com>
2020-10-27 09:17:58 +0800

    Halt MyAdapter

--- app/src/main/java/com/fareez/dummylistingapp/adapter/MyAdapter.kt
+++ app/src/main/java/com/fareez/dummylistingapp/adapter/MyAdapter.kt
@@ -15,26 +15,26 @@ import com.fareez.dummylistingapp.model.MovieModel
 import com.fareez.dummylistingapp.ui.MovieDetailsActivity
 import com.squareup.picasso.Picasso
 
-class MyAdapter(private val data: List<MovieModel.Details>, val context: Context) :
+class MyAdapter(private val data: List<MovieModel>, val context: Context) :
     RecyclerView.Adapter<MyAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
         //Binding UI with PlacesModel
-        fun bindItems(movieModel: MovieModel.Details){
-            val tvMovieTitle = itemView.findViewById(R.id.tv_movie_title) as TextView
-            val tvMovieYear = itemView.findViewById(R.id.tv_movie_year) as TextView
-            val ivMoviePoster = itemView.findViewById(R.id.iv_movie) as ImageView
-
-            tvMovieTitle.text = movieModel.Title
-            tvMovieYear.text = movieModel.Year.toString()
-            Picasso.get()
-                .load(movieModel.Poster)
-                .fit()
-                .error(android.R.drawable.ic_menu_report_image)
-                .into(ivMoviePoster)
-
-        }
+//        fun bindItems(movieModel: MovieModel.Details){
+//            val tvMovieTitle = itemView.findViewById(R.id.tv_movie_title) as TextView
+//            val tvMovieYear = itemView.findViewById(R.id.tv_movie_year) as TextView
+//            val ivMoviePoster = itemView.findViewById(R.id.iv_movie) as ImageView
+//
+//            tvMovieTitle.text = movieModel.Title
+//            tvMovieYear.text = movieModel.Year.toString()
+//            Picasso.get()
+//                .load(movieModel.Poster)
+//                .fit()
+//                .error(android.R.drawable.ic_menu_report_image)
+//                .into(ivMoviePoster)
+//
+//        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -43,20 +43,20 @@ class MyAdapter(private val data: List<MovieModel.Details>, val context: Context
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        data?.get(position)?.let { holder.bindItems(it) }
-
-        holder.itemView.setOnClickListener {
-
-            //get position selected item
-            val model = data.get(position)
-
-            //get movie id (imdbID)
-            val fetchedID: String = model.imdbID
-
-            val intent = Intent(context, MovieDetailsActivity::class.java)
-            intent.putExtra("movieID", fetchedID)
-            context.startActivity(intent)
-        }
+//        data?.get(position)?.let { holder.bindItems(it) }
+//
+//        holder.itemView.setOnClickListener {
+//
+//            //get position selected item
+//            val model = data.get(position)
+//
+//            //get movie id (imdbID)
+//            val fetchedID: String = model.imdbID
+//
+//            val intent = Intent(context, MovieDetailsActivity::class.java)
+//            intent.putExtra("movieID", fetchedID)
+//            context.startActivity(intent)
+//        }
     }
 
     override fun getItemCount(): Int {
